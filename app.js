4e344de6e5281558fdd09cc2e4e913f9c0bdd70e
haiaza <83097184+Haiaza@users.noreply.github.com>
2024-05-16 18:43:49 +0000

    Setting up Exercise 4

--- app.js
+++ app.js
@@ -47,3 +47,11 @@ const difficulty = "x";
 // console.log(game.difficulty);
 // bracket notation in the log show's the assignment side by side inside the difficulty property.
 console.log(game);
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
