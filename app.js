c8d2aa620316e50422dfbbbd2cad060e8c196a13
Haiaza <ajames1342@gmail.com>
2024-05-19 14:14:12 -0400

    Got it

--- app.js
+++ app.js
@@ -84,3 +84,18 @@ party.forEach(member => {
 })
 };
 partyHp();
+/*
+Exercise 7
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 7 here:
+*/
+
+game.gyms.forEach(gym => {
+  if (gym.difficulty < 3) {
+    gym.completed = true
+  }
+  console.log(game.gyms)
+});
