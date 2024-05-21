f1aa098aa71130c6a1e7d42d507e4f71386fc8c0
haiaza <83097184+Haiaza@users.noreply.github.com>
2024-05-21 17:59:42 +0000

    Q16 done. Setting up q17

--- app.js
+++ app.js
@@ -265,3 +265,16 @@ Solve Exercise 15 here:
 let monsterTally = 0;
 
 game.partyCount();
+/*
+Exercise 16
+1. Now, complete gyms with a difficulty below 8. Reflect on how this is similar to or different from the previous gym exercises.
+(change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 16 here:
+*/
+for (let i = 0; i < game.gyms.length; i++) {
+  if (game.gyms[i].difficulty < 8) {
+    game.gyms[i].completed = true;
+  }
+}
+console.log(game.gyms);
